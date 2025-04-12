e41538cdacabc4eef9d41e1a14e13f9317eb2d9d
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-04-12 21:56:37 +0100

    basic structure done

--- ch_01/sha1_cracker/src/main.rs
+++ ch_01/sha1_cracker/src/main.rs
@@ -1,11 +1,28 @@
-use std::env;
+use std::{env, error::Error, fs::File, io::{BufRead, BufReader}};
 
-fn main() {
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
         print!("Usage:");
         print!("sha1_cracker: <wordlist.txt> <sha1_hash>");
-        return;
+        return Ok(());
     }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
+    }
+    
+    Ok(())
 }
