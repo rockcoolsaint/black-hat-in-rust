28bac2dc5bce9f6d7339728c14fdef55c09158ae
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-04-13 13:27:31 +0100

    sha1_cracker wordlist traversal

--- ch_01/sha1_cracker/src/main.rs
+++ ch_01/sha1_cracker/src/main.rs
@@ -1,5 +1,7 @@
 use std::{env, error::Error, fs::File, io::{BufRead, BufReader}};
 
+use sha1::Digest;
+
 const SHA1_HEX_STRING_LENGTH: usize = 40;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -21,8 +23,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for line in reader.lines() {
         let line = line?.trim().to_string();
-        println!("{}", line);
+        let common_password = line.trim();
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+            println!("Password found: {}", &common_password);
+            return Ok(());
+        }
     }
+
+    println!("password not found in wordlist :(");
     
     Ok(())
 }
