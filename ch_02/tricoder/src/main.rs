dca28578e3b8e69fa4402cf0c08e1dac3c316fc1
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-04-19 22:09:49 +0100

    added multithreading to scanner

--- ch_02/tricoder/src/main.rs
+++ ch_02/tricoder/src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use thiserror::Error;
 
 mod subdomains;
@@ -9,7 +10,29 @@ pub enum Error {
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // we use custom threadpool to improve speed
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
 
+    // pool.install is required to use our custom threadpool, instead of rayon's default one
+    pool.install(|| {
+        let scan_result: Vec<Subdomain> = subdomains::enumerate(&http_client, target)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+
+        for subdomains in scan_result {
+            println!("{}:", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!(" {}", port.port);
+            }
+
+            println!("");
+        }
+    });
     Ok(())
 }
