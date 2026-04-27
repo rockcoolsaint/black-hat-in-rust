03419a30af8945d3db4a80e37da26106ba81ac56
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2026-04-27 20:30:30 +0100

    Delete ch_02/tricoder/target directory
    delete target

--- ch_02/tricoder/target/debug/deps/tricoder-054b041806ee987d.d
+++
@@ -1,6 +0,0 @@
-/home/innocent/Documents/practice/rusty/black-hat-rust/ch_02/tricoder/target/debug/deps/libtricoder-054b041806ee987d.rmeta: src/main.rs src/subdomains.rs
-
-/home/innocent/Documents/practice/rusty/black-hat-rust/ch_02/tricoder/target/debug/deps/tricoder-054b041806ee987d.d: src/main.rs src/subdomains.rs
-
-src/main.rs:
-src/subdomains.rs:
